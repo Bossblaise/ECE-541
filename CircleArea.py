c6a9f331af217799d3e39e91f27926945f9fc2c8
Bossblaise <97961383+Bossblaise@users.noreply.github.com>
2022-11-07 16:38:40 +0100

    Add files via upload

---
+++ CircleArea.py
@@ -0,0 +1,15 @@
+# This is a sample Python script to calculate the area of a circle
+
+# collect value of the radius
+radius = input("Enter the radius of the circle: ")
+
+# we assign the value of the Pi constant
+Pi = 3.142
+
+# Area of a circle = PI * Radius square
+# Store result in the variable "Area"
+Area = Pi * (float(radius)**2)
+
+# display the result
+print("The area of the circle is " + str(Area))
+
---
+++ StudentDetails.py
@@ -0,0 +1,6 @@
+# collection of input/information from the Student
+Name = input("What is your name? ")
+Age = input("How old are you: ")
+Department = input("What department are you in: ")
+# print out the information received
+print("Your name is " + Name + "\nYou are " + Age + "years old." + "\nYou are in the Department of  " + Department)
